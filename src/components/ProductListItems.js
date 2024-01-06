0d81c617f7d587cfa2387fd54ad656cb4abe0097
rajag <rajagurru333@gmail.com>
2024-01-07 01:22:19 +0530

    first commit

---
+++ src/components/ProductListItems.js
@@ -0,0 +1,33 @@
+// import React from 'react';
+
+// function ProductListItems(props) {
+  
+//   return (
+//     <div className='d-flex m-4 align-items-center justify-content-center'>
+//       <div className='mt-2 me-4'>
+//         <img
+//           src={props.thumbnail}
+//           height={150}
+//           width={180}
+//           alt={props.title} // Corrected from props.tittle
+//           className='border-radius-9'
+//         />
+//       </div>
+//       <h5 className='card-title me-4'>{props.title}</h5>
+//       <h6 className='mt-2 me-4'>Price Rs: {props.price}</h6>
+//       <h6 className='mt-2 me-4'>Rating: {props.rating}</h6>
+//       <button className='btn btn-danger ms-3' onClick={props.incrementItem}>
+//         +
+//       </button>
+// <span className='ms-3'>Quantity {Number(props.count)}</span>
+//       <button className='btn btn-danger ms-3' onClick={props.decrementItem}>
+//         -
+//       </button>
+//       <button className='btn btn-danger ms-3' onClick={props.removeItem}>
+//         Remove
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default ProductListItems;
